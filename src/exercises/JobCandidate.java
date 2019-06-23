f7d496bb06a8b4479ba62380ecdeac44dfbce85c
Kolinjo <kozicmarko@yahoo.com>
2019-06-23 23:01:35 +0200

    First change

--- src/exercises/JobCandidate.java
+++ src/exercises/JobCandidate.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 
 import java.util.List;
 
-public class JobCandidate {
+public class JobCandidate implements Comparable<JobCandidate> {
 
 	private List<String> personalityTraits;
 	private int hoursSpentCoding;
@@ -24,9 +24,51 @@ public class JobCandidate {
 		this.teacherName = teacherName;
 		this.salaryRequired = salaryRequired;
 	}
-	
+
 	public static void main(String[] args) {
-		JobCandidate june = new JobCandidate(Arrays.asList(new String[] {"adaptable", "scatty", "curious"}), 650, "Vinny Wade", 1200000); 
+		JobCandidate june = new JobCandidate(Arrays.asList(new String[] { "adaptable", "scatty", "curious" }), 650,
+				"Vinny Wade", 1200000);
+		JobCandidate marko = new JobCandidate(Arrays.asList(new String[] { "loyal", "friendly","hardworker"}),40,
+				"Andrija and June", 2000);
+
+		if (june.compareTo(marko) == 1) {
+			System.out.println(june.compareTo(marko));
+			System.out.println("June is a better candidate than Marko");
+		} else if (june.compareTo(marko) == -1) {
+			System.out.println("Marko is a better candidate than June");
+		} else {
+			System.out.println("Both candidates are equally good for this positiion.");
+		}
+		
+		
+		
 	}
+
+	@Override
+	public int compareTo(JobCandidate candidate) {
+		
+		final int BEFORE = -1;
+		final int EQUAL = 0;
+		final int AFTER = 1;
+		
+				
+		if (this.hoursSpentCoding>candidate.hoursSpentCoding) return AFTER;
+		if (this.hoursSpentCoding<candidate.hoursSpentCoding) return BEFORE;
+		if (this.hoursSpentCoding == candidate.hoursSpentCoding) return EQUAL;
+		
+		if (this.salaryRequired>candidate.salaryRequired) return AFTER;
+		if (this.salaryRequired<candidate.salaryRequired) return BEFORE;
+		if (this.salaryRequired == candidate.salaryRequired) return EQUAL;
+		
+		if (this.personalityTraits.size()>candidate.personalityTraits.size()) return AFTER;
+		if (this.personalityTraits.size()<candidate.personalityTraits.size()) return BEFORE;
+		if (this.personalityTraits.size()==candidate.personalityTraits.size()) return EQUAL;
+		
+		return EQUAL;
+		
+	}
+
+	
+	
 
 }
