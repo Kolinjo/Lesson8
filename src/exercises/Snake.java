539cae44a49f00c9b4b529a16435192730be9051
Kolinjo <kozicmarko@yahoo.com>
2019-06-23 23:05:30 +0200

    First change

---
+++ src/exercises/Snake.java
@@ -0,0 +1,25 @@
+package exercises;
+
+public class Snake {
+
+	private boolean venomous = true;
+
+	public Snake() {
+		this.venomous = venomous;
+	}
+
+	public boolean isVenomous() {
+		return venomous;
+	}
+
+	public void setVenomous(boolean venomous) {
+		this.venomous = venomous;
+	}
+
+	
+	
+	
+	
+	
+	
+}
